6c25b717d8ab758125a6deb83b4b650026f77aac
coding.he <229101253@qq.com>
2020-07-14 16:29:24 +0800

    1.0.0

--- src/com/coding/tool/Main.java
+++ src/com/coding/tool/Main.java
@@ -2,8 +2,6 @@ package com.coding.tool;
 
 import com.coding.tool.view.MainWindow;
 
-import java.io.File;
-
 /**
  * @author: Coding.He
  * @date: 2020/7/14
@@ -12,7 +10,6 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] arg) {
-        System.out.println(new File("./").getAbsolutePath());
         new MainWindow();
     }
 }
